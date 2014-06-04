0a84524ad8e14e1aaaf589069cce5891c45db21a
git-justinpodur <git-justin@podur.org>
2014-06-04 13:06:43 -0400

    completed exercise 6.3

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,14 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each {|item| print "*" + item + "\n"}
+#exercise 6.2
+grocery_list.push("rice")
+
+#exercise 6.2
+def display_list(my_list)
+	my_list.each {|item| print "*" + item + "\n"}
+end
+
+display_list(grocery_list)
+
+#exercise 6.3
+puts "The number of items on your list is #{grocery_list.length} items"
